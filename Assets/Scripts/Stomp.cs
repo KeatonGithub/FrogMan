9f29da954fcd9ae95cc474034ea9fffa3918bf77
KeatonGithub <keatonkp7777@gmail.com>
2023-12-15 13:34:28 -0500

    Added comments to the Stomp script

--- Assets/Scripts/Stomp.cs
+++ Assets/Scripts/Stomp.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class Stomp : MonoBehaviour
 {
 
-    public float bounce;
-    public Rigidbody2D rigBod2d;
+    public float bounce; // declare value for how high the player flies
+    public Rigidbody2D rigBod2d; //delcare the collider for the FrogMans feet
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +15,12 @@ public class Stomp : MonoBehaviour
     }
 
     // Update is called once per frame
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D Goomba) //setting up the player collision with the slime
     {
-        if (other.CompareTag("Slime"))
+        if (Goomba.CompareTag("Slime")) //when player collides with the Slime object delete that object
         {
-            Destroy(other.gameObject);
-            rigBod2d.velocity = new Vector2(rigBod2d.velocity.x,bounce);
+            Destroy(Goomba.gameObject);//destroys the game slime object
+            rigBod2d.velocity = new Vector2(rigBod2d.velocity.x,bounce); //when player collides with object bonce upwards
         }
     }
 }
