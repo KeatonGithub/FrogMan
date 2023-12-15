da102c863ee0a34586eba48985c5ea53cd28570c
KeatonGithub <keatonkp7777@gmail.com>
2023-12-15 13:41:47 -0500

    Added comments for the Camera Station script

--- Assets/Scripts/CameraStation.cs
+++ Assets/Scripts/CameraStation.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class CameraStation : MonoBehaviour
 {
-    [SerializeField] private Transform player;
+    [SerializeField] private Transform player;//declare the players position so that the camera can track it later
+    //serialized field lets us edit the function in engine if the function is private
    
     void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z); 
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z); //create a vector 3 that tracks the players position and set the camera to be equal to it
     }
 }
