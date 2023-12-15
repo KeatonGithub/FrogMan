cae9a90d65843c08d7389a25c50d14e2d334a0e8
KeatonGithub <keatonkp7777@gmail.com>
2023-12-15 14:38:54 -0500

    Better Formating

--- Assets/Scripts/PlayerTriggers.cs
+++ Assets/Scripts/PlayerTriggers.cs
@@ -7,12 +7,12 @@ public class PlayerTriggers : MonoBehaviour
     public WinLose winLoseScript;
     void Update()
     {
-      if(transform.position.y < -12.0f)//when the player falls off trigger the lose level script
+        if (transform.position.y < -12.0f)//when the player falls off trigger the lose level script
         {
             winLoseScript.LoseLevel();//trigger the loselevel script
         }
 
-      if(transform.position.x > 16.5f)//when the player hits the x coordinate at the end of the level they win
+        if (transform.position.x > 16.5f)//when the player hits the x coordinate at the end of the level they win
         {
             winLoseScript.WinLevel();//trigger the winlevel script
         }
--- Assets/Scripts/PlayersMovement.cs
+++ Assets/Scripts/PlayersMovement.cs
@@ -5,30 +5,30 @@ using UnityEngine;
 
 public class PlayersMovement : MonoBehaviour
 {
-   private Rigidbody2D rb2d;//declare the RigidBody2D as a variable
-   public BoxCollider2D colliders;//declaring colliders as a variable
-    //Serialized field lets us change values in unity without it having to be public
-   [SerializeField] private Animator animations; //uses animator function for sprites
-   [SerializeField] private SpriteRenderer sprite;//uses spriterenderer function for sprites
-   private float moveSpeed = 7f;//movespeed
-   private float jumpForce = 14f;//jumpspeed
-   private float directionOfX = 0f;//direction
-   [SerializeField] private LayerMask groundjumping;//lets us now if the player is on the ground layer
+    private Rigidbody2D rb2d;//declare the RigidBody2D as a variable
+    public BoxCollider2D colliders;//declaring colliders as a variable
+                                   //Serialized field lets us change values in unity without it having to be public
+    [SerializeField] private Animator animations; //uses animator function for sprites
+    [SerializeField] private SpriteRenderer sprite;//uses spriterenderer function for sprites
+    private float moveSpeed = 7f;//movespeed
+    private float jumpForce = 14f;//jumpspeed
+    private float directionOfX = 0f;//direction
+    [SerializeField] private LayerMask groundjumping;//lets us now if the player is on the ground layer
 
     // Start is called before the first frame update
-   private void Start()
+    private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();//Get and declare the Rigidbody2D, BoxCollider, Animator, and SpriteRenderer
         colliders = GetComponent<BoxCollider2D>();
         animations = GetComponent<Animator>();
-        sprite = GetComponent<SpriteRenderer>();   
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
-   private void Update()
+    private void Update()
     {
         directionOfX = Input.GetAxisRaw("Horizontal");//gets the input for x
-        rb2d.velocity = new Vector2 (directionOfX * moveSpeed, rb2d.velocity.y); //lets us customize the velcity
+        rb2d.velocity = new Vector2(directionOfX * moveSpeed, rb2d.velocity.y); //lets us customize the velcity
 
         if (Input.GetButtonDown("Jump") && IsColliding()) //When Space bar is pressed you jump! Also checks if you are on the ground so you can jump
         {
@@ -59,6 +59,6 @@ public class PlayersMovement : MonoBehaviour
 
     private bool IsColliding()//this function is here so that you cannot spam jump to fly so it checks if the player is colliding with the terrain
     {
-       return Physics2D.BoxCast(colliders.bounds.center, colliders.bounds.size, 0f, Vector2.down, .1f, groundjumping);
+        return Physics2D.BoxCast(colliders.bounds.center, colliders.bounds.size, 0f, Vector2.down, .1f, groundjumping);
     }
 }
